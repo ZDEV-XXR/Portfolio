475eb6e93c6891c6b7d47d098c131b9fdae5301b
ZDEV-XXR <contact.hamzalemghari@gmail.com>
2026-05-07 16:50:13 +0100

    you can download app demo directly as .apk file

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -26,23 +26,23 @@ export const projects: Project[] = [
     description: "A high-performance Android news aggregator that fetches real-time global headlines. Implemented custom caching logic to allow offline reading and utilized Retrofit for seamless API integration.",
     techStack: ["Android", "Jetpack Compose", "Firebase", "Kotlin", "Retrofit"],
     githubUrl: "https://github.com/ZDEV-XXR/InstantNews",
-    demoUrl: "https://portal.testapp.io/apps/install/6ebVxWXaL4WmA",
+    demoUrl: "./downloads/instantnews.apk",
     image: "./assets/in.png",
     category: "Mobile"
   },
   {
     id: "3",
-    title: "DocReader ( In Progress )",
+    title: "DocReader",
     description: "Read, scan Documents",
     techStack: ["Android", "Jetpack Compose", "Kotlin"],
     githubUrl: "https://github.com/ZDEV-XXR/DocReader",
-    demoUrl: "",
+    demoUrl: "./downloads/docreader.apk",
     image: "./assets/dr.png",
     category: "Mobile"
   },
   {
     id: "4",
-    title: "Upwork Job Radar ( staged to be published )",
+    title: "Upwork Job Radar",
     description: "Upwork Job Radar is a Chrome extension that helps freelancers stay updated on the latest job postings on Upwork. It provides real-time notifications for new job listings based on user-defined criteria, allowing freelancers to quickly respond to relevant opportunities and increase their chances of securing projects.",
     techStack: ["HTML5 & CSS3", "JavaScript", "Manifest V3 (MV3)"],
     githubUrl: "https://github.com/ZDEV-XXR/Upwork-Job-Radar",
